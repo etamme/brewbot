02d09f81595c1d5a0ec281769e9190c7ee217e34
Eric Tamme <etamme@gmail.com>
2011-08-03 11:40:37 -0400

    changed tinyurl len to 65 with the goal of only preventing line wrap for console irc clients

--- tinyurl.rb
+++ tinyurl.rb
@@ -9,7 +9,7 @@ class Tinyurl
   include Cinch::Plugin
   match(/(http.+)/,{:use_prefix => false})
   def execute(m,url)
-     if(url.length>50 and (!url.include?("tinyurl")))
+     if(url.length>65 and (!url.include?("tinyurl")))
        tinyurl=Net::HTTP.get(URI.parse("http://tinyurl.com/api-create.php?url=#{url}"))
        m.reply tinyurl
      end
