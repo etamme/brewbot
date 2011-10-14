e7e40a9c26ce7d44941c8e99d70dae4cb5e4ed12
Eric Tamme <etamme@gmail.com>
2011-10-14 09:45:44 -0400

    GA release of google docs integrated yeastbot

--- gsyeast.rb
+++ gsyeast.rb
@@ -38,10 +38,26 @@ class GSYeast
     super
     # these are the current columns we are using - in order
     # MFG#,lab,product name, species, brewery source, temp range, attenuation range, flocculation, alcohol tolerance, pitching/ferm notes, notes, styles, same as, available, citation
-    
+    @map={} 
+    @map['MFG']=1
+    @map['LAB']=2
+    @map['PRODUCT']=3
+    @map['SPECIES']=4
+    @map['SOURCE']=5
+    @map['TEMP']=6
+    @map['ATTENUATION']=7
+    @map['FLOCCULATION']=8
+    @map['TOLERANCE']=9
+    @map['PITCH_NOTES']=10
+    @map['NOTES']=11
+    @map['STYLES']=12
+    @map['SAME_AS']=13
+    @map['AVAIL']=14
+    @map['CITE']=15
+
     @key="0AmRc5_x3ehAfdFhBQ3pmczhqdHUtbmFONUYyZzVEY0E"
-    @user="hbtbrewbot@gmail.com"
-    @pass="gfx10101"
+    @user="foo@gmail.com"
+    @pass="pass"
     @session=nil
     @ws=nil
     @index={}
@@ -49,11 +65,40 @@ class GSYeast
     reloadData()
   end
 
-  def getYeast(yeast)
+  def getYeast(yeast,cmd)
     row=getRow(yeast)
     data=""
-    for col in 1..@ws.num_cols
-          data="#{data} "+@ws[row, col]
+    # our default set of columns to return, along with their abbreviatted display elements.
+    default={'PRODUCT'=>'','ATTENUATION'=>'att:', 'TOLERANCE'=>'tol:','TEMP'=>'temp:','FLOCCULATION'=>'flocc:','SOURCE'=>'src:','SAME_AS'=>'eqv:'}
+    if cmd==nil
+       # loop through each column and check that is has data.
+       default.each do |k,v|
+         if @ws[row,@map[k]] !=''
+           if v !=""
+             tag="#{v} "
+           else
+             tag=""
+           end
+
+           if data==""
+             data="#{tag}#{@ws[row,@map[k]]}"
+           else
+            data="#{data} #{tag}#{@ws[row,@map[k]]}"
+           end
+         end
+       end
+    elsif cmd!='HELP'
+      if @map[cmd]
+        if @ws[row, @map[cmd]] != ""
+          data="#{data} "+@ws[row, @map[cmd]]
+        else
+          data="no #{cmd.downcase} information"
+        end
+      else
+        data="Unable to find matching command, please use !yeast #{yeast} help"
+      end
+    else
+      data="!yeast MFG### [help|lab|product|species|source|temp|attenuation|flocculation|tolerance|pitch_notes|notes|styles|same_as|avail|cite]"
     end
     return data
   end
@@ -61,12 +106,15 @@ class GSYeast
   def execute(m,yeast)
     yeast,cmd = yeast.split(' ')
     yeast.upcase!
+    if cmd!=nil
+      cmd.upcase!
+    end
     puts "got #{yeast} -  #{cmd}"
-    puts @index
+    #puts @index
     if yeast=="RELOAD"
       reloadData()
     elsif getRow(yeast) != nil
-      m.reply "#{m.user.nick}, #{yeast}: #{getYeast(yeast)}"
+      m.reply "#{m.user.nick}, #{yeast}: #{getYeast(yeast,cmd)}"
     else
       m.reply "#{m.user.nick}, you be crazy fool. #{yeast} doesn't exist!"
     end
