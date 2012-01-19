1a03105d0a424733a76442e17cbf49e7db26f899
Eric Tamme <etamme@gmail.com>
2012-01-19 12:56:48 -0500

    new twitter command - thanks to ikonis being a RUBY MASTAR

---
+++ twitter.rb
@@ -0,0 +1,23 @@
+#!/usr/local/bin/ruby
+require 'net/http'
+require 'uri'
+require 'cinch'
+require 'rubygems'
+require 'xmlsimple'
+
+class Twitter
+  include Cinch::Plugin
+
+  match /twitter (.+)/
+
+  def execute(m,screenname)
+    screenname=screenname.gsub(' ','+')
+    uri="http://api.twitter.com/1/statuses/user_timeline.xml?include_entities=true&include_rts=false&screen_name=#{screenname}&count=1"
+    puri=URI.parse(uri)
+    xml=Net::HTTP.get(puri)
+    data = XmlSimple.xml_in(xml)
+    status = data["status"][0]
+    tweet = status["text"][0]
+    m.reply "@#{screenname}: #{tweet}"
+  end
+end
