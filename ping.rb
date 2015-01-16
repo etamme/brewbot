d03dca0d5066550f5f1b94433dc1cd908b146373
Colin Burton <gremlyn1@gmail.com>
2015-01-16 11:15:42 -0500

    Hopefully this works!

--- ping.rb
+++ ping.rb
@@ -7,12 +7,8 @@ class Ping
   @help="."
   match(/^\.$/,{:use_prefix => false})
   def execute(m)
-    if(m.bot.nick != "homebrewbot")
-      if(m.bot.user_list.find("homebrewbot"))
-        return 0
-      else
-        m.bot.nick="homebrewbot"
-      end
+    if(m.bot.nick != "homebrewbot" && !m.bot.user_list.find("homebrewbot"))
+      m.bot.nick="homebrewbot"
     end
    m.reply ".." 
   end
