47ea5e16389ad32b64483efc16f9a2ebe9dfa958
Gremlyn <gremlyn1@gmail.com>
2015-08-13 13:17:05 -0400

    Fixed Convert!!!

--- convert.rb
+++ convert.rb
@@ -1,42 +1,20 @@
 #!/usr/local/bin/ruby
 require 'cinch'
-require 'net/http'
-require 'uri'
+require 'ruby-units'
 
 class Convert
   include Cinch::Plugin
   @help="!convert"
   match(/convert (.+)/)
-  def execute(m,arg)
-    if(m.bot.nick != "homebrewbot")
-      if(m.bot.user_list.find("homebrewbot"))
-        return 0
-      else
-        m.bot.nick="homebrewbot"
-      end
-    end
+  def execute(m,convert)
+    
+    units = convert.split(' to ')
 
-       lhs=""
-       rhs=""
-       arg=arg.gsub(' ','+')
-       data=Net::HTTP.get(URI.parse("http://www.google.com/ig/calculator?q=#{arg}"))
-       # {lhs: "1 pound",rhs: "16 ounces",error: "",icc: false}
-       data=data.gsub('{','')
-       data=data.gsub('}','')
-       kvps=data.split(',')
-       kvps.each do |kvp|
-         kvp=kvp.gsub('"','')
-         arr = kvp.split(':')
-         if arr[0]=="lhs"
-            lhs=arr[1]
-         elsif arr[0]=="rhs"
-            rhs=arr[1]
-         end
-       end
-       if lhs!=" " && rhs!=" "
-         m.reply "#{lhs} is #{rhs}"
-       else
-         m.reply "Are you sure those measurements exist?"
-       end
+    unit0 = Unit(units[0])
+    if unit0.compatible?(units[1])
+      unit1 = unit0 >> units[1]
+      
+      m.reply "#{m.user.nick}, #{unit0} is #{unit1}"
+    end
   end
 end
