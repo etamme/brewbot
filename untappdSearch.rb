14d9a235dd92c483c48858d8ef5535c282f9a8dc
Gremlyn <gremlyn1@gmail.com>
2015-08-13 12:17:08 -0400

    Switch to Untappd for scores.

---
+++ untappdSearch.rb
@@ -0,0 +1,72 @@
+#!/usr/local/bin/ruby
+
+require 'rubygems'
+require 'cinch'
+require 'open-uri'
+require 'uri'
+require 'yaml'
+
+class UntappdSearch
+  include Cinch::Plugin
+  @help="!beerscore"
+  @untappd_urls=[]
+  match /beerscore (.+)/
+  
+  def initialize(*args)
+    super
+    # load config from yaml file
+    @untappd_config = YAML::load_file('untappd.yaml')
+
+    @id = @untappd_config['id']
+    @secret = @untappd_config['secret']
+  end
+  
+  def getURL(node, params)
+    urls = {
+      "beer_search" => "https://api.untappd.com/v4/search/beer",
+      "beer_score"  => "https://api.untappd.com/v4/beer/info/"
+    }
+    url = urls[node]
+    query = "?client_id=#{@id}&client_secret=#{@secret}"
+    
+    if(params.is_a?(Hash))
+        params.each do |param,value|
+          query = query + "&#{param}=#{value}"
+        end
+    else
+      url = url + params
+    end
+    
+    url = url + query
+    
+    return url
+  end
+  
+  def execute(m,beer)
+    if(m.bot.nick != "homebrewbot")
+      if(m.bot.user_list.find("homebrewbot"))
+        return 0
+      else
+        m.bot.nick="homebrewbot"
+      end
+    end
+
+    params = {
+      'q' => beer,
+      'limit' => '5'
+    }
+    
+    search_url = getURL('beer_search',params)
+    
+    results = JSON.load(URI.parse(URI.encode(search_url)))
+    
+    results["response"]["beers"]["items"].each do |result|
+      score_url = getURL('beer_score', result["beer"]["bid"].to_s)
+      score = JSON.load(URI.parse(URI.encode(score_url)))
+      
+      rating = score["response"]["beer"]["rating_score"]
+      
+      m.reply "#{rating.round(2)} for #{result["beer"]["beer_name"]} by #{result["brewery"]["brewery_name"]}"
+    end
+  end
+end
