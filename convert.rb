ac4d785d154ad37c3cc3d0ceda4b0a036769a032
Gremlyn <gremlyn1@gmail.com>
2015-11-18 09:29:26 -0500

    Fixing up convert to use the new() method from ruby units. Also redefined temp units.

--- convert.rb
+++ convert.rb
@@ -6,11 +6,27 @@ class Convert
   include Cinch::Plugin
   @help="!convert"
   match(/convert (.+)/)
+  
+  def initialize(*args)
+    super
+    Unit.redefine!("tempC") do |tempC|
+      tempC.display_name = "C"
+    end
+    
+    Unit.redefine!("tempF") do |tempF|
+      tempF.display_name = "F"
+    end
+    
+    Unit.redefine!("tempK") do |tempK|
+      tempK.display_name = "K"
+    end
+  end
+
   def execute(m,convert)
     
     units = convert.split(' to ')
 
-    unit0 = Unit(units[0])
+    unit0 = Unit.new(units[0])
     if unit0.compatible?(units[1])
       unit1 = unit0 >> units[1]
       
