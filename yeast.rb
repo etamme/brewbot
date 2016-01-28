4085d5461f06f63a781f8535a4b77b41779c9a02
Gremlyn <gremlyn1@gmail.com>
2016-01-28 11:13:13 -0500

    Search strain, then name for yeast.

--- yeast.rb
+++ yeast.rb
@@ -15,17 +15,31 @@ class Yeast
     
   end
   
+  def searchYeast(params)
+    uri = URI.parse("http://brewerwall.com/api/v1/yeasts")
+    response = Net::HTTP.post_form(uri,params)
+      
+    results = JSON.load(response.body)
+      
+    return results
+  end
+  
   def execute(m,yeast)
     if (yeast != ' ')
       params = {
         'strain' => yeast
       }
+            
+      results = searchYeast(params)
       
-      uri = URI.parse("http://brewerwall.com/api/v1/yeasts")
-      response = Net::HTTP.post_form(uri,params)
-      
-      results = JSON.load(response.body)
-      
+      if (results.empty?)
+        params = {
+          'name' => yeast
+        }
+        
+        results = searchYeast(params)
+      end
+
       m.reply "#{results[0]["laboratory"]} #{results[0]["strain"]}: #{results[0]["name"]}; Temperature: #{results[0]["temperature_min"]}-#{results[0]["temperature_max"]}; Attenuation: #{results[0]["attenuation_min"]}-#{results[0]["attenuation_max"]}; Tolerance: #{results[0]["tolerance"]}%; Flocculation: #{results[0]["flocculation"]}"
     end
   end
