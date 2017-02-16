6c4846f7d49910cfb8cede72b02aa31b12882b4c
Gremlyn <gremlyn1@gmail.com>
2017-02-16 12:15:11 -0500

    Make negative numbers convert I think

--- convert.rb
+++ convert.rb
@@ -6,16 +6,16 @@ class Convert
   include Cinch::Plugin
   @help="!convert"
   match(/convert (.+)/)
-  
+
   def initialize(*args)
     super
-    
+
     Unit.define("bbl") do |unit|
       unit.aliases = "barrel"
       unit.definition = Unit.new('31 gallons')
       unit.display_name = "bbl"
     end
-    
+
     Unit.define("beers") do |unit|
       unit.aliases = %w{bottles beer-bottles}
       unit.definition = Unit.new('12 floz')
@@ -24,25 +24,25 @@ class Convert
   end
 
   def execute(m,convert)
-    
+
     units = convert.split(' to ')
 
-    re = /(\d+(:?\.\d+)?)\s*([CFK])/
+    re = /(^-?\d+(:?\.\d+)?)\s*([CFK])/
     match = units[0].match re
     puts match
     if (match)
       last = match.captures.length
       temp = match.captures[0]
       temp_unit = match.captures[last-1]
-      
+
       units[0] = "#{temp} temp#{temp_unit}"
       units[1] = "temp#{units[1]}"
     end
-    
+
     unit0 = Unit.new(units[0])
     if unit0.compatible?(units[1])
       unit1 = unit0 >> units[1]
-      
+
       m.reply "#{m.user.nick}, #{unit0} is #{unit1.to_s('%0.2f')}"
     end
   end
